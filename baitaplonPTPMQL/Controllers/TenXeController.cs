f94ce747863f626daf626f552e9281341155f160
H1921050280 <phamhung235z@gmail.com>
2023-05-21 21:37:36 +0700

    update

--- baitaplonPTPMQL/Controllers/TenXeController.cs
+++ baitaplonPTPMQL/Controllers/TenXeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MvcMovie.Data;
 using baitaplonPTPMQL.Models;
+using baitaplonPTPMQL.Models.Process;
 
 namespace baitaplonPTPMQL.Controllers
 {
@@ -159,5 +160,55 @@ namespace baitaplonPTPMQL.Controllers
         {
           return (_context.TenXe?.Any(e => e.XeID == id)).GetValueOrDefault();
         }
+         private ExcelProcess _excelProcess = new ExcelProcess();
+
+        public async Task<IActionResult> Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>Upload(IFormFile file)
+        {
+            if (file!=null)
+            {
+                string fileExtension = Path.GetExtension(file.FileName);
+                if (fileExtension != ".xls" && fileExtension != ".xlsx")
+                {
+                    ModelState.AddModelError("", "Please choose excel file to upload!");
+                }
+                else
+                {
+                    //rename file when upload to sever
+                    var fileName = DateTime.Now.ToShortTimeString() + fileExtension;
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory() + "/Uploads/Excels", fileName);
+                    var fileLocation = new FileInfo(filePath).ToString();
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        //save file to server
+                        await file.CopyToAsync(stream);
+                        //read data from file and write to database
+                        var dt = _excelProcess.ExcelToDataTable(fileLocation);
+                        //dùng vòng lặp for để đọc dữ liệu dạng hd
+                        for (int i = 0; i < dt.Rows.Count; i++)
+                        {
+                            //create a new Student object
+                            var TX = new TenXe();
+                            //set values for attribiutes
+                            TX.XeID = dt.Rows[i][0].ToString();
+                            TX.TenXe_BienSo = dt.Rows[i][1].ToString();
+                            //add oject to context
+                            _context.TenXe.Add(TX);
+                        }
+                        //save to database
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            return View();
+        
     }
-}
+ }  
+    }
+
